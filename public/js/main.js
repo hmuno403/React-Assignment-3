47019dff53df5c81dba15523dbbbe6a9bad935ef
hmuno403 <124557027+hmuno403@users.noreply.github.com>
2023-03-20 16:24:45 -0600

    stylimg

--- public/js/main.js
+++ public/js/main.js
@@ -82,4 +82,22 @@ themeButton.addEventListener('click', () => {
 })
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== CONTAINER ===============*/
+
+
+const panels = document.querySelectorAll ('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click',()=> {
+        removeActiveClasses()
+        panel.classList.add ('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active') 
+
+    })
+
+}
